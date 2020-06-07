bdc4adfc698b88ab2ce9532daaa563c40edc5aed
Mathias Tejeda <mathi.tejeda@gmail.com>
2020-06-06 21:52:23 -0300

    Agregar archivos de proyecto.

---
+++ swim/main.cpp
@@ -0,0 +1,36 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
+#include <SDL.h>
+#include <iostream>
+
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+int main(int argc, char* args[]) {
+	SDL_Window* window = NULL;
+	SDL_Surface* screen_surface = NULL;
+	
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) std::cout << "No se pudo inicializar SDL. " << SDL_GetError();
+
+	else {
+		window = SDL_CreateWindow("Swim!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		
+		if (window == NULL) std::cout << "No se pudo crear la ventana. " << SDL_GetError();
+
+		else {
+			screen_surface = SDL_GetWindowSurface(window);
+			SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface(window);
+			SDL_Delay(2000);
+		}
+	}
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+
+	return 0;
+}
+
